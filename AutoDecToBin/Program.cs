3763dbe64864665f46ef65e5d233664e6d8e9d9c
brojlibrojli <brojlibrojli@gmail.com>
2020-09-26 19:36:13 +0200

    1 commit do AutoDecToBin

---
+++ AutoDecToBin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ManualDecToBin
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Insert Integer: ");
+            int value = 0;
+            bool isNumber = int.TryParse(Console.ReadLine(), out value);
+            while (!isNumber)
+            {
+                Console.WriteLine("It is no number");
+                isNumber = int.TryParse(Console.ReadLine(), out value);
+            }
+            while (isNumber)
+            {
+                string binary = Convert.ToString(value, 2);
+                Console.Write("Decimal to Binary: " + binary + "\n");
+                isNumber = int.TryParse(Console.ReadLine(), out value);
+
+            }
+        }
+    }
+}
